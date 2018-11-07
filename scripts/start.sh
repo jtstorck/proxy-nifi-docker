debcaf65114b3c42d77df8298688286d8271ef8d
Jeff Storck <jtswork@gmail.com>
2018-11-07 00:09:25 -0500

    Added docker-compose-anonymous-nifi.yaml which creates traefik and nifi containers Added an example nifi topology to be used by Knox Added a start script which clones Sandeep More's knox-dev-docker repository and uses docker-compose to bring up all the containers

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in#answer-246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR=$SCRIPT_DIR/..
+
+# clone Snadeep's repo, which creates docker containers to compile Knox,
+# start an LDAP, and start Knox
+git clone https://github.com/moresandeep/knox-dev-docker.git $REPO_DIR/knox-dev-docker
+
+# copy the NiFi topology into the cloned knox-dev-docker repo
+cp $REPO_DIR/topologies/nifi.xml $REPO_DIR/knox-dev-docker/topologies
+
+docker-compose -f $REPO_DIR/knox-dev-docker/docker-compose.yml up -d
+
+docker-compose -f $REPO_DIR/docker-compose-anonymous-nifi.yaml up -d
